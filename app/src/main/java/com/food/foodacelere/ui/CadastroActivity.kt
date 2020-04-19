d38100074ba08d93a31c5e208033d58e7acc15b0
Barkokebas <allana.b.barkokebas@accenture.com>
2020-04-19 14:25:04 -0300

    Adiciona tela para quando esquecer email

---
+++ app/src/main/java/com/food/foodacelere/ui/CadastroActivity.kt
@@ -0,0 +1,13 @@
+package com.food.foodacelere.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.food.foodacelere.R
+
+class CadastroActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
--- app/src/main/java/com/food/foodacelere/ui/LoginActivity.kt
+++ app/src/main/java/com/food/foodacelere/ui/LoginActivity.kt
@@ -1,13 +1,27 @@
 package com.food.foodacelere.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.food.foodacelere.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lblRegistrar.setOnClickListener {
+            val intent = Intent(this,CadastroActivity::class.java)
+            startActivity(intent)
+        }
+
+        lblEsqueceuSenha.setOnClickListener {
+            val intent = Intent(this,ForgotPassword::class.java)
+            startActivity(intent)
+        }
     }
 }
+
